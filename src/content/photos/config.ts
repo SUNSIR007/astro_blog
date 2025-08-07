1a9d34b4cb66667a5f3713938eef2b9b28fc6e0a
Ryuichi_wsl <2540987771@qq.com>
2025-08-07 11:54:48 +0800

    进一步调整

--- src/content/photos/config.ts
+++ src/content/photos/config.ts
@@ -1,14 +1,9 @@
 import { defineCollection, z } from 'astro:content'
 
 export const photoSchema = z.object({
-  title: z.string(),
-  description: z.string().optional(),
   url: z.string().url(),
   thumbnail: z.string().url().optional(),
-  alt: z.string(),
   date: z.date(),
-  tags: z.array(z.string()).optional(),
-  featured: z.boolean().optional().default(false),
 })
 
 export const photos = defineCollection({
